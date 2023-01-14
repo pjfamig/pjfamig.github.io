1b8cd8131f9ce3aa158603ca52930075db40b967
famiglietti <p0seid0n_@hotmail.com>
2023-01-14 13:03:17 -0800

    Update banner and content

---
+++ js/analytics.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZX3BCE97DG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZX3BCE97DG');
+</script>
