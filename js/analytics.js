f3861a6c138eb3644084916ed63851b42261fad3
famiglietti <p0seid0n_@hotmail.com>
2023-02-01 09:10:16 -0800

    Delete js directory

--- js/analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZX3BCE97DG"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZX3BCE97DG');
-</script>
